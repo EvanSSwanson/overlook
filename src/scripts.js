42effd1f010093044c0c72acb78fc4f1dd5f764a
Evan Swanson <evan.sundelius@gmail.com>
2022-11-08 21:59:49 -0600

    Initial commit

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,6 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
+//Initial commit!
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
